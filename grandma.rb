cb3a1da60958a633c13517c0f440a77c82c3841a
George Ruiz <georuiz817@gmail.com>
2019-05-14 17:29:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,4 +2,16 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     end
+  if phrase == phrase.downcase 
+    return "HUH? ! SPEAK UP, SONNY!"
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
   end
